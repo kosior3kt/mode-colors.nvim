59e8c2beb073ad95a109f9f74fdfba45da81f16f
Jakub Kosiorek <jakkos69@gmail.com>
2024-09-07 13:09:04 +0200

    some more weird changes

--- lua/color_modes/init.lua
+++ lua/color_modes/init.lua
@@ -3,38 +3,42 @@ local M = {}
 
 local config = require("color_modes.config")
 
-M.debugPrint = function(msg)
+debugPrint = function(msg)
     print(msg)
 end
 
-M.user_colors = {}
+user_colors = {}
+
+local verbose = false
 
 function M.setup(_usr_config)
-    M.user_colors = vim.tbl_deep_extend("force", config.default_colors, _usr_config or {})
+    user_colors = vim.tbl_deep_extend("force", config.default_colors, _usr_config or {})
 end
 
 ---so this is a black magic to a degree - we need to wait before execution, cause some jumps take time - 20ms should be UwU
 function changeColour()
     vim.defer_fn(function()
         local mode = vim.api.nvim_get_mode().mode
-        local color = M.user_colors.cursor_color[mode] or "none"
+        local color = user_colors.cursor_color[mode] or "none"
 
         vim.api.nvim_command("hi! cursorline guifg=none guibg=" .. color)
 
-        if M.user_colors.column_marker and M.user_colors.column_marker.use then
-            vim.api.nvim_command(":set colorcolumn=" .. M.user_colors.column_marker.number)
-            vim.api.nvim_command("hi! ColorColumn guifg=none guibg=" .. M.user_colors.column_marker.colour)
+        if user_colors.column_marker and user_colors.column_marker.use then
+            vim.api.nvim_command(":set colorcolumn=" .. user_colors.column_marker.number)
+            vim.api.nvim_command("hi! ColorColumn guifg=none guibg=" .. user_colors.column_marker.colour)
         end
 
         vim.api.nvim_command(":highlight CursorColumn guibg=" .. color)
+
+         if verbose == true then
+            debugPrint("colour changed")
+         end
+
     end, 20)
 end
 
-changeColour()
-
 vim.api.nvim_command([[autocmd ModeChanged * lua changeColour()]])   
--- vim.api.nvim_command([[autocmd CursorMoved * lua changeColour()]])  -- this one is only for the first mode change
 
-
+changeColour() -- initial invoke
 return M
 
